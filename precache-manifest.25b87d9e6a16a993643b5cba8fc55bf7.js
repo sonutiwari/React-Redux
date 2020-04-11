f1a415fb463670b64c060505a2352e85b7289536
Sonu Tiwari <ankorha@gmail.com>
2020-04-12 04:02:34 +0530

    Updates

---
+++ precache-manifest.25b87d9e6a16a993643b5cba8fc55bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2401f17fdd94769b2c9a4afa68dc096",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "e222aa518f00a50446b3",
+    "url": "/React-Redux/static/css/main.601aea70.chunk.css"
+  },
+  {
+    "revision": "e10dbaef59b0858c632c",
+    "url": "/React-Redux/static/js/2.9cd80998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Redux/static/js/2.9cd80998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e222aa518f00a50446b3",
+    "url": "/React-Redux/static/js/main.dbf12548.chunk.js"
+  },
+  {
+    "revision": "4646e0fcb58014ced4ee",
+    "url": "/React-Redux/static/js/runtime-main.e298bd86.js"
+  }
+]);
